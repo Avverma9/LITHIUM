c6dd29164997a0c52112289c28977166eba368eb
Avverma9 <av95766@gmail.com>
2022-10-15 12:29:53 +0530

    Its My second assignment

---
+++ src/routes/logger.js
@@ -0,0 +1,8 @@
+let printSomething = function() {
+    console.log("Welcome to my application. I am Deeptirtha Mukherjee and a part of FunctionUp Lithium cohort.")
+    return "done"
+}
+
+
+
+module.exports.myFunction = printSomething
