a3b1999aab5da1ae597ad3c6409ee5350c549eb3
Avverma9 <av95766@gmail.com>
2022-10-20 16:22:03 +0530

    this is my post api assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,73 +1,110 @@
 const express = require('express');
-const router = express.Router();
-
-router.get('/students/:name', function(req, res) {
-    let studentName = req.params.name
-    console.log(studentName)
-    res.send(studentName)
-})
-
-router.get("/random" , function(req, res) {
-    res.send("hi there")
-})
-
-
-router.get("/test-api" , function(req, res) {
-    res.send("hi FunctionUp")
-})
-
-
-router.get("/test-api-2" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API")
-})
-
-
-router.get("/test-api-3" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's ")
-})
-
-
-router.get("/test-api-4" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
-
-
-
-router.get("/test-api-5" , function(req, res) {
-    res.send("hi FunctionUp5. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
-
-router.get("/test-api-6" , function(req, res) {
-    res.send({a:56, b: 45})
-})
-
-router.post("/test-post", function(req, res) {
-    res.send([ 23, 45 , 6])
-})
+const app = express.Router();
+const router=express.Router();
+/*//Q1.
+// -write an api which gives the missing number in an array of integers starting from 1….e.g [1,2,3,5,6,7]
+//: 4 is missing
+// Your route code will look like this
+app.get('/missingNumber', (req, res) => {
+  const arr = [1, 2, 3, 5, 6, 7];
+  let missingNumber = 0;
+  for (let i = 0; i < arr.length; i++) {
+      if (arr[i] + 1 !== arr[i + 1]) {
+          missingNumber = arr[i] + 1;
+          break;
+      }
+  }
+  res.send(`The missing number is ${missingNumber}`);
+});
+// Q2.
+// -write an api which gives the missing number in an array of integers starting from anywhere….e.g [33,
+//34, 35, 37, 38]: 36 is missing
+// Your route code will look like this
+app.get('/missingNumber1', (req, res) => {
+  const arr = [33, 34, 35, 37, 38];
+  let missingNumber = 0;
+  for (let i = 0; i < arr.length; i++) {
+      if (arr[i] + 1 !== arr[i + 1]) {
+          missingNumber = arr[i] + 1;
+        break;
+      }
+       }
+  res.send(`The missing number is ${missingNumber}`);
+});
 
 
-router.post("/test-post-2", function(req, res) {
-    res.send(  { msg: "hi" , status: true }  )
-})
+let players = [
+  {
+    name: "Ankit",
+    dob: "20/08/1998",
+    gender: "male",
+    city: "Patna",
+    sports: ["Cricket"],
+  },
+  {
+    name: "Akash",
+    dob: "10/01/2000",
+    gender: "male",
+    city: "Patna",
+    sports: ["Volleyball"],
+  },
+  {
+    name: "Abhishek",
+    dob: "05/11/2002",
+    gender: "male",
+    city: "Patna",
+    sports: ["swimming"],
+  }
+];
+router.post("/players",function (req, res) {
+  console.log(players)
+  let newplayer = req.body;
+  let found=req.body.name;
+  console.log("New Players Found :- ",newplayer)
+  for (i of players) {
+      if (i.name == found) {
+          return res.send("name already exists")   
+      }
+  }
+  players.push(newplayer)
+  res.send({ msg: players, status: true });
+});*/
 
-router.post("/test-post-3", function(req, res) {
-    // let id = req.body.user
-    // let pwd= req.body.password
 
-    // console.log( id , pwd)
-
-    console.log( req.body )
-
-    res.send(  { msg: "hi" , status: true }  )
-})
-
-
-
-router.post("/test-post-4", function(req, res) {
-    let arr= [ 12, "functionup"]
-    let ele= req.body.element
-    arr.push(ele)
-    res.send(  { msg: arr , status: true }  )
-})
-
+let persons= [
+  {
+  name: "PK",
+  age: 10,
+  votingStatus: false
+},
+{
+  name: "SK",
+  age: 20,
+  votingStatus: false
+},
+{
+  name: "AA",
+  age: 70,
+  votingStatus: false
+},
+{
+  name: "SC",
+  age: 5,
+  votingStatus: false
+},
+{
+  name: "HO",
+  age: 40,
+  votingStatus: false
+}
+]
+router.post("/persons",function (req, res) {
+let votingAge=req.query.votingAge
+/*let eligibleAge = persons.filter (ele => ele.age>votingAge)*/
+let eligibleAge=[]
+for (let i = 0; i < persons.length; i++) {
+     if (persons[i].age>votingAge)  eligibleAge.push(persons[i])
+}
+  res.send({data: eligibleAge ,status: true});
+});
 module.exports = router;
