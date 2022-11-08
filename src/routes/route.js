e3c940515b7695423f24289096046e82108ba66b
Avverma9 <av95766@gmail.com>
2022-11-09 00:36:18 +0530

    this is my axios-promises assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -9,11 +9,12 @@ router.get("/test-me", function (req, res) {
 })
 
 
-router.get("/cowin/states", CowinController.getStates)
-router.get("/cowin/districtsInState/:stateId", CowinController.getDistricts)
-router.get("/cowin/getByPin", CowinController.getByPin)
+router.get("/weatherDetails", CowinController.getWeather)
+router.get("/cowin/districtsInState", CowinController.getDistrictsAndDate) // 
+router.post("/memes", CowinController.memesById)
 
-router.post("/cowin/getOtp", CowinController.getOtp)
+/*router.post("/cowin/getOtp", CowinController.getOtp)
+router.post("/memes",CowinController.getMemes)*/
 
 // WRITE A GET API TO GET THE LIST OF ALL THE "vaccination sessions by district id" for any given district id and for any given date
 
